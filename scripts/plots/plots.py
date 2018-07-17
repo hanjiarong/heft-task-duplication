f669dbcd07b9ff13c1694ce6aee3105288c11c88
Thiago Genez <thiagogenez@gmail.com>
2018-07-17 19:16:40 +0200

    up

--- scripts/plots/plots.py
+++ scripts/plots/plots.py
@@ -11,15 +11,145 @@ import matplotlib.ticker as mtick
 
 
 from matplotlib.ticker import AutoMinorLocator
+from matplotlib.ticker import FuncFormatter
 
-def plot_bars(data, ccrs, algorithms):
+def y_fmt(y, pos):
+	decades = [1e9, 1e6, 1e3, 1e0, 1e-3, 1e-6, 1e-9 ]
+	suffix  = ["G", "M", "k", "" , "m" , "u", "n"  ]
+	if y == 0:
+		return str(0)
+	for i, d in enumerate(decades):
+		if np.abs(y) >=d:
+			val = y/float(d)
+			signf = len(str(val).split(".")[1])
+			if signf == 0:
+				return '{val:d} {suffix}'.format(val=int(val), suffix=suffix[i])
+			else:
+				if signf == 1:
+					if str(val).split(".")[1] == "0":
+						return '{val:d} {suffix}'.format(val=int(round(val)), suffix=suffix[i]) 
+				tx = "{"+"val:.{signf}f".format(signf = signf) +"} {suffix}"
+				return tx.format(val=val, suffix=suffix[i])
+	return y
+
+
+def get_correct_legend(labels):
+
+	new_labels = list()
+
+
+	for label in labels:
+
+		if 'HEFT' == label:
+			new_labels.append(r'HEFT')
+
+		elif 'HEFT-TaskDuplication' == label:
+			new_labels.append(r'TaskDuplication')			
+
+		elif 'HEFT-LookAhead-TaskDuplication' == label:
+			new_labels.append(r'LookAhead-TaskDuplication')
+
+		elif 'HEFT-Ilia-W-0.05' == label:
+			new_labels.append(r'W $0.05$')
+
+		elif 'HEFT-Ilia-W-0.10' == label:
+			new_labels.append(r'W $0.10$')
+
+		elif 'HEFT-Ilia-W-0.50' == label:
+			new_labels.append(r'W $0.50$')
+
+		elif 'HEFT-Ilia-W-0.90' == label:
+			new_labels.append(r'W $0.90$')
+
+		else:
+			new_labels.append('Unknown ylabel')
+
+
+	return new_labels
+
+def plot_errorbars(data, ccrs, algorithms, title='Title', ylabel='y_label', plot_filename='/tmp/plot', format='pdf'):
+
+	#fig, ax = plt.subplots()
+	fig, ax = plt.subplots(figsize=(13, 6))
+
+	minorLocator = AutoMinorLocator()
+
+	x = np.arange(len(ccrs))
+	errorbars = list()
+
+	#confg
+	linestyle = ['solid', 'dashed', 'dashdot', 'dotted', 'solid']
+	markers = ['v', 'o', 'x', 'D', '*']
+
+	#preparing data
+	
+	for i in range(len(algorithms)):	
+		y = list()
+		yerr = list()
+
+		algorithm = algorithms[i]
+
+		for ccr in ccrs:	
+
+			mean = data[ccr][algorithm]['mean']
+			ci = data[ccr][algorithm]['ci']
+
+			y.append(mean)
+			yerr.append(ci)
+
+		errorbar = ax.errorbar(x, y, yerr=yerr, ls=linestyle[i], marker=markers[i], color='black', markerfacecolor="None", capsize=5)
+		errorbars.append(errorbar[0])
+
+	#################
+	# x-axis config #
+	#################
+	ax.xaxis.labelpad = 15
+	ax.set_xticks(x,ccrs)
+	ax.set_xlabel(r'CCR', fontsize=14)
+
+	#################
+	# y-axis config #
+	#################
+	ax.set_ylabel(ylabel, fontsize=14)
+	ax.yaxis.labelpad = 9
+	ax.yaxis.set_minor_locator(minorLocator)
+	ax.yaxis.set_major_formatter(FuncFormatter(y_fmt))
+	
+
+	#################
+	# general config#
+	#################
+	
+	ax.set_title(title, fontsize=17)
+
+	# Hide these grid behind plot objects
+	ax.set_axisbelow(True)
+
+	# Legend
+	plt.legend(errorbars, get_correct_legend(algorithms), loc='upper left', ncol=1, numpoints=2, fontsize=10)
+
+
+	
+	# tight layout
+	plt.tight_layout(True)
+	
+	#grid
+	ax.grid('on', axis='both', ls='dashed')
+
+	#################
+	#     saving    #
+	#################
+	plt.savefig( '%s-errobar.%s' % (plot_filename, format), format=format, bbox_inches='tight') #dpi=300,  rasterized=True)		
+	plt.clf()
+	plt.close('all')
+
+def plot_bars(data, ccrs, algorithms, title='Title', ylabel='y_label', plot_filename='/tmp/plot', format='pdf'):
 
 	# plot
 	fig, ax = plt.subplots(figsize=(13, 6))
+	#fig, ax = plt.subplots()
 
 	# variables
-	ylabel = 'y_label'
-	title = 'title'
 	number_of_bars = len(algorithms)
 	minorLocator = AutoMinorLocator()
 	
@@ -52,18 +182,21 @@ def plot_bars(data, ccrs, algorithms):
 	# bar configs
 	rects = list()
 	colors = ['red', 'lightsalmon',  'lightgreen', 'royalblue', 'yellowgreen', 'deepskyblue', 'silver', 'silver']
-	opacity = 0.9
+	colors = ['white', 'white',  'white', 'black', 'white', 'deepskyblue', 'silver', 'silver']
+	opacity = 0.8
 	error_config = {'ecolor': '0.0'}
-	#patterns = ('..',  'x', 'xx',  '--')
+	patterns = (' ', '..',  'x',  '--', 'xxx', '\\', '+', '.')
 
 	# creating rects
 	for i in range(number_of_bars):
 		rect = ax.bar(bar_positions[i::number_of_bars], bar_means[i::number_of_bars], bar_width,
-			#hatch=patterns[i],
+			hatch=patterns[i],
 			alpha=1.0,
 			color=colors[i],
 			yerr=bar_cis[i::number_of_bars],
 			error_kw=error_config,
+			capsize=5,
+			edgecolor='black',
 			label='%s' % (algorithms[i]))
 		rects.append(rect)
 
@@ -72,8 +205,8 @@ def plot_bars(data, ccrs, algorithms):
 	#################
 	
 	# ensembles tick labels
-	ensemble_xticklabels = [r'$%s$' % (i)  for i in ccrs]
-	ensemble_xticks = map(lambda x: x  + ((number_of_bars * bar_width)/2), bar_positions[::number_of_bars])
+	xticklabels = [r'$%s$' % (i)  for i in ccrs]
+	xticks = map(lambda x: x  + ((number_of_bars * bar_width)/2), bar_positions[::number_of_bars])
 
 	
 	# divisor line between ensenbles sizes
@@ -81,32 +214,54 @@ def plot_bars(data, ccrs, algorithms):
 
 
 	# set ticks and labels
-	ax.set_xticks( ensemble_xticks)
+	ax.set_xticks( xticks)
 	ax.set_xticks( divisor_xticks, minor=True )
-	ax.set_xticklabels(ensemble_xticklabels, fontsize=20)
-	v_y = [-0.03] * len(ensemble_xticklabels)
+	ax.set_xticklabels(xticklabels, fontsize=14)
+	v_y = [-0.03] * len(xticklabels)
 	for t, y in zip( ax.get_xticklabels(), v_y ):
 		t.set_y(y)
 
 	ax.xaxis.labelpad = 15
-	ax.set_xlabel(r'CCR', fontsize=20)
+	ax.set_xlabel(r'CCR', fontsize=15)
 	ax.grid('off', axis='x', which='minor')
-	ax.tick_params( axis='x', which='minor', direction='out', length=30, top='off')
+	ax.tick_params( axis='x', which='minor', direction='out', length=0, top='off')
 	ax.tick_params( axis='x', which='major', bottom='on', top='off', length=2 )
 	ax.set_xlim(right=bar_positions[-1] + bar_width )
 
+	#################
+	# y-axis config #
+	#################
+
+	ax.set_ylabel(ylabel, fontsize=14)
+	ax.yaxis.labelpad = 9
+	ax.yaxis.grid(color='black',linestyle='dotted', linewidth=1)
+	ax.yaxis.set_minor_locator(minorLocator)
+	ax.yaxis.set_major_formatter(FuncFormatter(y_fmt))
+	ax.tick_params( axis='both', labelsize=14)
 
 	#################
 	# general config#
 	#################
 	
+	ax.set_title(title, fontsize=17)
+
 	# Hide these grid behind plot objects
 	ax.set_axisbelow(True)
 
+	# Legend
+	plt.legend(rects, get_correct_legend(algorithms), loc='upper left', ncol=1, fontsize=10)
+
+	
+	# tight layout
+	plt.tight_layout(True)
 
 
-
-	plt.show()
+	#################
+	#     saving    #
+	#################
+	plt.savefig( '%s-bar.%s' % (plot_filename, format), format=format, bbox_inches='tight') #dpi=300,  rasterized=True)		
+	plt.clf()
+	plt.close('all')
 
 def mean_confidence_interval(data, confidence=0.95):
     a = 1.0*np.array(data)
@@ -135,7 +290,7 @@ def calculate_stats(data, ccrs, algorithms):
 
 	return data
 
-def parse_json(json_filename, ccrs, algorithms):
+def parse_json(json_filename, ccrs, algorithms,  title='title', plot_filename='plot', output_dir='/tmp'):
 
 	makespans =  create_data_field(ccrs, algorithms)
 	communications =  create_data_field(ccrs, algorithms)
@@ -155,14 +310,16 @@ def parse_json(json_filename, ccrs, algorithms):
 						for result in raw_data:
 							
 							algorithm = result['flavour']
-							
-							makespan = result['makespan']
-							runtime = result['runtime']
-							communication = result['totalBytesSent']
 
-							makespans[ccr][algorithm]['values'].append(makespan)
-							runtimes[ccr][algorithm]['values'].append(runtime)
-							communications[ccr][algorithm]['values'].append(communication)
+							if algorithm in algorithms:
+							
+								makespan = result['makespan']
+								runtime = result['runtime']
+								communication = result['totalBytesSent']
+
+								makespans[ccr][algorithm]['values'].append(makespan)
+								runtimes[ccr][algorithm]['values'].append(runtime)
+								communications[ccr][algorithm]['values'].append(communication)
 
 
 			except (ValueError) as e:
@@ -177,21 +334,25 @@ def parse_json(json_filename, ccrs, algorithms):
 	communications = calculate_stats(communications, ccrs, algorithms)
 	runtimes = calculate_stats(runtimes, ccrs, algorithms)
 
-	plot_bars(makespans, ccrs, algorithms)
-	plot_bars(communications, ccrs, algorithms)
+	plot_errorbars(makespans, ccrs, algorithms, ylabel='Average makespan', title=title, plot_filename='%s/makespan-%s' % (output_dir, plot_filename))
+	plot_bars(makespans, ccrs, algorithms, ylabel='Average makespan', title=title, plot_filename='%s/makespan-%s' % (output_dir, plot_filename))
+
+	plot_errorbars(communications, ccrs, algorithms, ylabel='Average communication cost', title=title, plot_filename='%s/communication-%s' % (output_dir, plot_filename))
+	plot_bars(communications, ccrs, algorithms, ylabel='Average communication cost', title=title, plot_filename='%s/communication-%s' % (output_dir, plot_filename))
+
+	#plot_bars(runtimes, ccrs, algorithms, ylabel='Average Execution time (s)', title=title, plot_filename='%s/runtime-%s' % (output_dir, plot_filename))
 
 
 if __name__ == '__main__':
 
 
-	algorithms = ['HEFT','HEFT-TaskDuplication','HEFT-LookAhead-TaskDuplication','HEFT-Ilia-W-0.05', 'HEFT-Ilia-W-0.10', 'HEFT-Ilia-W-0.50', 'HEFT-Ilia-W-0.90']
+	algorithms = ['HEFT','HEFT-Ilia-W-0.05', 'HEFT-TaskDuplication','HEFT-LookAhead-TaskDuplication'] #,'HEFT-Ilia-W-0.10', 'HEFT-Ilia-W-0.50', 'HEFT-Ilia-W-0.90']
 	app_names =  ['MONTAGE', 'CYBERSHAKE', 'GENOME', 'LIGO', 'SIPHT']
 	app_sizes = ['50', '100', '300', '500', '1000']
 	vm_files = ['heft.2.yaml', 'heft.10.yaml']
 	ccrs = ['0.1', '0.5', '1.0', '1.5', '2.0']
 
 	data_dir = '/local1/thiagogenez/mulitple-workflow-simulation'
-	plots_dir = '/local1/thiagogenez/mulitple-workflow-simulation'
 
 
 	parser = argparse.ArgumentParser(description='Simulator runner', add_help=True, prog='run.py', usage='python %(prog)s [options]', epilog='Mail me (thiagogenez@ic.unicamp.br) for more details', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -200,9 +361,9 @@ if __name__ == '__main__':
 	parser.add_argument('--vm_files', nargs='+', help='VM files', type=int, action='store', default=vm_files)
 	parser.add_argument('--algorithms', nargs='+', type=str, help='Scheduling policies', action='store', default=algorithms)
 	parser.add_argument('--version', action='version', version='%(prog)s 1.0')
-	parser.add_argument('--ccrs', nargs='+', help='communication to computation ratios (CCR)', type=float, action='store', default=ccrs)
+	parser.add_argument('--ccrs', nargs='+', help='communication to computation ratios (CCR)', type=str, action='store', default=ccrs)
 	parser.add_argument('--data_dir', nargs='?', help='Simulation data', type=str, action='store', default=data_dir)
-	parser.add_argument('--plots_dir', nargs='?', help='Simulation data', type=str, action='store', default=plots_dir)
+	
 
 	# parsing arguments
 	try:
@@ -212,12 +373,21 @@ if __name__ == '__main__':
 
 
 	# prepare dir to receive the plot files
-	output_dir = '%s/plots' % (args.plots_dir)
+	output_dir = '%s/plots' % (args.data_dir)
 	mkdir(output_dir)
 
 
 	for app_name in args.app_names:
 		for app_size in args.app_sizes:
 			for vm_file in args.vm_files:
+				
 				json_filename= '%s/%s/%s/%s/results/simulation.json' % (args.data_dir, app_name, app_size, vm_file)
-				parse_json(json_filename, args.ccrs, args.algorithms)
+				
+				title = '%s with %s tasks' % (app_name, app_size)
+				plot_filename = '%s-%s' % (app_name, app_size)
+				if 'heft.2.yaml' in vm_file:
+					title = '%s - 2 resources' % (title)
+				elif 'heft.10.yaml' in vm_file:
+					title = '%s - 10 resources' % (title)
+
+				parse_json(json_filename, args.ccrs, args.algorithms, title=title, plot_filename=plot_filename, output_dir=output_dir)
